cf2c0dcc7f9c7e1b267d3f384ee42278cd665d97
Simon Bull <simon.bull.84@googlemail.com>
2018-01-11 14:44:31 +0000

    Make static infrastructure for users

---
+++ src/infrastructure/users/static.js
@@ -0,0 +1,43 @@
+const users = [
+  {
+    name: 'Wade Wilson',
+    email: 'deadpool@x-force.test',
+    organisation: {
+      name: 'X-Force'
+    },
+    lastLogin: new Date(2018, 0, 11, 11, 30, 57),
+    status: {
+      description: 'Active'
+    }
+  },
+  {
+    name: 'Frank Castle',
+    email: 'punisher@forcerecon.test',
+    organisation: {
+      name: 'Force Recon'
+    },
+    lastLogin: new Date(2018, 0, 11, 12, 15, 00),
+    status: {
+      description: 'Active'
+    }
+  },
+];
+
+const search = async (criteria) => {
+  const results = users.filter(u => u.name.toLowerCase().indexOf(criteria.toLowerCase()) > -1
+    || u.email.toLowerCase().indexOf(criteria.toLowerCase()) > -1
+    || u.organisation.name.toLowerCase().indexOf(criteria.toLowerCase()) > -1);
+  return Promise.resolve(results.sort((x, y) => {
+    if (x < y) {
+      return -1;
+    }
+    if (x > y) {
+      return 1;
+    }
+    return 0;
+  }));
+};
+
+module.exports = {
+  search,
+};
