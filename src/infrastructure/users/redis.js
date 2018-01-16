725b83b98cc55dbacf18c7fd7da29ae688d15a9c
Simon Bull <simon.bull.84@googlemail.com>
2018-01-16 17:06:04 +0000

    Update redis connector to support multiple indexes

--- src/infrastructure/users/redis.js
+++ src/infrastructure/users/redis.js
@@ -2,15 +2,17 @@ const redis = require('redis');
 const { promisify } = require('util');
 const config = require('./../config');
 const { concat } = require('lodash');
+const uuid = require('uuid/v4');
 
 const client = redis.createClient({
   url: config.cache.params.connectionString,
 });
 const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 const scanAsync = promisify(client.scan).bind(client);
 
-const getPage = async (criteria, pointer) => {
-  const result = await scanAsync(pointer, 'MATCH', `*${criteria.toLowerCase()}*`);
+const getPage = async (criteria, pointer, indexName) => {
+  const result = await scanAsync(pointer, 'MATCH', `${indexName}-*${criteria.toLowerCase()}*`);
 
   const nextPagePointer = result[0];
   const users = await Promise.all(result[1].map(async (key) => {
@@ -26,11 +28,15 @@ const getPage = async (criteria, pointer) => {
 };
 
 const search = async (criteria) => {
-  let results = [];
+  const indexName = await getAsync('CurrentIndex');
+  if (!indexName) {
+    return [];
+  }
 
+  let results = [];
   let nextPagePointer = 0;
   do {
-    const page = await getPage(criteria, nextPagePointer);
+    const page = await getPage(criteria, nextPagePointer, indexName);
     results = concat(results, page.users);
     nextPagePointer = page.nextPagePointer;
   } while (nextPagePointer > 0);
@@ -39,15 +45,19 @@ const search = async (criteria) => {
 };
 
 const createIndex = async () => {
-  return Promise.resolve('new');
+  return Promise.resolve(uuid());
 };
 
 const updateIndex = async (users, index) => {
-  return Promise.resolve(null);
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    const key = `${index}-${user.name}:${user.email}:${user.organisation ? user.organisation.name : ''}`;
+    await setAsync(key, JSON.stringify(user));
+  }
 };
 
 const updateActiveIndex = async (index) => {
-  return Promise.resolve(null);
+  await setAsync('CurrentIndex', index);
 };
 
 module.exports = {
