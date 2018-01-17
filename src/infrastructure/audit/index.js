0956a9578a88c5df9f1607e6e1e0bacb74112b3d
Simon Bull <simon.bull.84@googlemail.com>
2018-01-17 15:05:15 +0000

    Redis audit adapter

--- src/infrastructure/audit/index.js
+++ src/infrastructure/audit/index.js
@@ -1,19 +1,12 @@
-const getUserAudit = async (userId, pageNumber) => {
-  return Promise.resolve({
-    audits: [{
-      type: "sign-in",
-      subType: "username-password",
-      success: true,
-      userId: "7a1b077a-d7d4-4b60-83e8-1a1b49849510",
-      userEmail: "some.user@test.tester",
-      level: "audit",
-      message: "Successful login attempt for some.user@test.tester (id: 7a1b077a-d7d4-4b60-83e8-1a1b49849510)",
-      timestamp: "2017-10-24T12:35:51.633Z"
-    }],
-    numberOfPages: 1,
-  });
-};
+const config = require('./../config');
 
-module.exports = {
-  getUserAudit,
-};
+let adapter;
+if (config.audit.type === 'static') {
+  adapter = require('./static');
+} else if (config.audit.type === 'redis') {
+  adapter = require('./redis');
+} else {
+  throw new Error(`Invalid audit type ${config.audit.type} in config`);
+}
+
+module.exports = adapter;
