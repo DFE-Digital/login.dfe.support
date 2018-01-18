e84ee1494c62af8f6b69543de6464f4e1bf2bbed
Simon Bull <simon.bull.84@googlemail.com>
2018-01-18 10:10:41 +0000

    Orgs api

--- src/infrastructure/organisations/index.js
+++ src/infrastructure/organisations/index.js
@@ -3,6 +3,8 @@ const config = require('./../config');
 let adapter;
 if (config.organisations.type === 'static') {
   adapter = require('./static');
+} else if(config.organisations.type === 'api') {
+  adapter = require('./api');
 } else {
   throw new Error(`Invalid organisations type ${config.directories.type} in config`);
 }
