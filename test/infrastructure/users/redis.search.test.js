6d1d9673ea62bf575e6bcbc2fe26ef3adeb919a8
Simon Bull <simon.bull.84@googlemail.com>
2018-01-19 15:49:48 +0000

    Default sort order

--- test/infrastructure/users/redis.search.test.js
+++ test/infrastructure/users/redis.search.test.js
@@ -36,7 +36,7 @@ const user2 = JSON.stringify({
   },
 });
 
-describe('when searching for users', () => {
+describe('when searching for users in redis', () => {
   let get;
   let set;
   let scan;
@@ -117,28 +117,28 @@ describe('when searching for users', () => {
     expect(get.mock.calls[2][0]).toBe('testindex-user-key-2');
   });
 
-  it('then it should return all users from scan with last login translated to a date', async () => {
+  it('then it should return all users from scan with last login translated to a date, ordered by name if no order specified', async () => {
     const actual = await search('test', 1);
 
     expect(actual.users).toHaveLength(2);
     expect(actual.users[0]).toMatchObject({
-      name: 'Timmy Tester',
-      email: 'timmy@tester.test',
-      organisation: {
-        name: 'Testco'
-      },
-      lastLogin: new Date(2018, 0, 11, 11, 30, 57),
-      status: {
-        description: 'Active'
-      },
-    });
-    expect(actual.users[1]).toMatchObject({
       name: 'Brenda Breaker',
       email: 'brenda@breakage.test',
       organisation: {
         name: 'Break Inc'
       },
       lastLogin: new Date(2018, 0, 11, 12, 15, 0),
+      status: {
+        description: 'Active'
+      },
+    });
+    expect(actual.users[1]).toMatchObject({
+      name: 'Timmy Tester',
+      email: 'timmy@tester.test',
+      organisation: {
+        name: 'Testco'
+      },
+      lastLogin: new Date(2018, 0, 11, 11, 30, 57),
       status: {
         description: 'Active'
       },
