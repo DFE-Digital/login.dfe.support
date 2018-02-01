07ea4addc1c110d26c65e243ae188a59364139ff
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-01 16:02:08 +0000

    Add delete for indexes for UserDevices

--- src/infrastructure/userDevices/azureSearch.js
+++ src/infrastructure/userDevices/azureSearch.js
@@ -9,6 +9,7 @@ const client = redis.createClient({
   url: config.cache.params.indexPointerConnectionString,
 });
 
+const getAsync = promisify(client.get).bind(client);
 const setAsync = promisify(client.set).bind(client);
 
 const azureSearch = require('./../azureSearch');
@@ -51,9 +52,24 @@ const updateActiveIndex = async (index) => {
   await setAsync('CurrentIndex_UserDevices', index)
 };
 
+const deleteUnusedIndexes = async () => {
+  const currentIndexName = await getAsync('CurrentIndex_UserDevices');
+
+  // Delete any indexes already marked as unused
+  const unusedJson = await getAsync('UnusedIndexes_UserDevices');
+  const unusedIndexes = unusedJson ? JSON.parse(unusedJson) : [];
+  await azureSearch.deleteUnusedIndexes(unusedIndexes, currentIndexName);
+
+  // Find any remaining indexes that appear to be unused
+  const indexesResponse = await azureSearch.getIndexes();
+  const indexesAppearingUnused = indexesResponse.value.map(x => x.name).filter(x => x !== currentIndexName && x.toLowerCase().indexOf('userdevices-') !== -1);
+  await setAsync('UnusedIndexes_UserDevices', JSON.stringify(indexesAppearingUnused));
+};
+
 module.exports = {
   createIndex,
   updateIndex,
   updateActiveIndex,
+  deleteUnusedIndexes,
 };
 
