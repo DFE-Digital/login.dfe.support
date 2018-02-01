e713b4f1967098ebbc2aaf291251c175f7dbf65c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-01 14:48:48 +0000

    Add create index func for UserDevices Search

--- src/infrastructure/userDevices/azureSearch.js
+++ src/infrastructure/userDevices/azureSearch.js
@@ -2,7 +2,10 @@
 
 const redis = require('redis');
 const { promisify } = require('util');
+const rp = require('request-promise');
 const config = require('./../config');
+const uuid = require('uuid/v4');
+const logger = require('./../logger');
 
 const client = redis.createClient({
   url: config.cache.params.indexPointerConnectionString,
@@ -10,6 +13,22 @@ const client = redis.createClient({
 
 const setAsync = promisify(client.set).bind(client);
 
+const azureSearch = require('./../azureSearch');
+
+const createIndex = async () => {
+  const fields = [
+    { name: 'id', type: 'Edm.String', key: true, searchable: false },
+    { name: 'deviceId', type: 'Edm.String', key: true, searchable: false },
+    { name: 'deviceStatus', type: 'Edm.String', searchable: false, filterable: true },
+    { name: 'serialNumber', type: 'Edm.String', key: true, searchable: true },
+    { name: 'name', type: 'Edm.String', sortable: true, filterable: true, searchable: true },
+    { name: 'email', type: 'Edm.String', sortable: true, filterable: true, searchable: true },
+    { name: 'organisationName', type: 'Edm.String', sortable: true, filterable: true, searchable: true },
+    { name: 'lastLogin', type: 'Edm.Int64', sortable: true, filterable: true },
+  ];
+  return await azureSearch.createIndex(`userdevices-${uuid()}`, fields);
+
+};
 
 const updateActiveIndex = async (index) => {
   await setAsync('CurrentIndex_UserDevices', index)
