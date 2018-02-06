d2fc06667d912c1cc611fb6920d7d6fcbd75f561
Simon Bull <simon.bull.84@googlemail.com>
2018-02-06 14:07:00 +0000

    Update status change in index

--- src/app/users/postConfirmDeactivate.js
+++ src/app/users/postConfirmDeactivate.js
@@ -1,11 +1,24 @@
 const logger = require('./../../infrastructure/logger');
 const { getUserDetails } = require('./utils');
 const { deactivate } = require('./../../infrastructure/directories');
+const { getById, updateIndex } = require('./../../infrastructure/users');
+
+const updateUserIndex = async (uid) => {
+  const user = await getById(uid);
+  user.status = {
+    description: 'Deactivated',
+  };
+  if (user.lastLogin) {
+    user.lastLogin = user.lastLogin.getTime();
+  }
+  await updateIndex([user]);
+};
 
 const postConfirmDeactivate = async (req, res) => {
   const user = await getUserDetails(req);
 
   await deactivate(user.id, req.id);
+  await updateUserIndex(user.id);
 
   logger.audit(`${req.user.email} (id: ${req.user.sub}) deactivated user ${user.email} (id: ${user.id})`, {
     type: 'support',
