ead117dd2bcf17f54c2cad9d33505b7f4dddb0ec
Simon Bull <simon.bull.84@googlemail.com>
2018-02-08 15:52:53 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/app/users/index.js
+++ src/app/users/index.js
@@ -14,6 +14,8 @@ const getConfirmDeactivate = require('./getConfirmDeactivate');
 const postConfirmDeactivate = require('./postConfirmDeactivate');
 const getConfirmReactivate = require('./getConfirmReactivate');
 const postConfirmReactivate = require('./postConfirmReactivate');
+const getNewUserK2S = require('./getNewUserK2S');
+const postNewUserK2S = require('./postNewUserK2S');
 
 const router = express.Router({ mergeParams: true });
 
@@ -25,6 +27,9 @@ const users = (csrf) => {
 
   router.get('/', csrf, getSearch);
   router.post('/', csrf, postSearch);
+
+  router.get('/new-k2s-user', csrf, getNewUserK2S);
+  router.post('/new-k2s-user', csrf, postNewUserK2S);
 
   router.get('/:uid', (req, res) => {
     res.redirect(`/users/${req.params.uid}/services`);
---
+++ test/infrastructure/organisations/api.getPageOfOrganisations.test.js
@@ -0,0 +1,82 @@
+jest.mock('request-promise');
+jest.mock('login.dfe.jwt-strategies');
+jest.mock('./../../../src/infrastructure/config', () => require('./../../utils').configMockFactory({
+  organisations: {
+    type: 'api',
+    service: {
+      url: 'http://organisations.test',
+    },
+  },
+}));
+
+const rp = require('request-promise');
+const jwtStrategy = require('login.dfe.jwt-strategies');
+const { getPageOfOrganisations } = require('./../../../src/infrastructure/organisations/api');
+
+const correlationId = 'abc123';
+const apiResponse = {
+  organisations: [{
+    id: 'org1',
+    name: 'org one',
+  }],
+  page: 1,
+  totalNumberOfPages: 2,
+};
+
+describe('when getting a page of organisations from api', () => {
+  beforeEach(() => {
+    rp.mockReset();
+    rp.mockImplementation(() => {
+      return apiResponse;
+    });
+
+    jwtStrategy.mockReset();
+    jwtStrategy.mockImplementation(() => {
+      return {
+        getBearerToken: jest.fn().mockReturnValue('token'),
+      };
+    })
+  });
+
+  it('then it should call organisations resource with page number', async () => {
+    await getPageOfOrganisations(2, correlationId);
+
+    expect(rp.mock.calls).toHaveLength(1);
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      method: 'GET',
+      uri: 'http://organisations.test/organisations?page=2',
+    });
+  });
+
+  it('then it should use the token from jwt strategy as bearer token', async () => {
+    await getPageOfOrganisations(2, correlationId);
+
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      headers: {
+        authorization: 'bearer token',
+      },
+    });
+  });
+
+  it('then it should include the correlation id', async () => {
+    await getPageOfOrganisations(2, correlationId);
+
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      headers: {
+        'x-correlation-id': correlationId,
+      },
+    });
+  });
+
+  it('then it should return page of orgs from api', async () => {
+    const actual = await getPageOfOrganisations(2, correlationId);
+
+    expect(actual).not.toBeNull();
+    expect(actual.totalNumberOfPages).toBe(2);
+    expect(actual.organisations).toHaveLength(1);
+    expect(actual.organisations[0]).toMatchObject({
+      id: 'org1',
+      name: 'org one',
+    });
+  });
+});
