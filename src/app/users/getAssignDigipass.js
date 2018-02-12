f6993c07a1e242c0d582a3b63afc2a74fd48369e
Simon Bull <simon.bull.84@googlemail.com>
2018-02-12 20:51:45 +0000

    Re-display serial number if go back to digipass

--- src/app/users/getAssignDigipass.js
+++ src/app/users/getAssignDigipass.js
@@ -2,10 +2,11 @@ const getAssignDigipass = (req, res) => {
   if (!req.session.k2sUser) {
     return res.redirect('../');
   }
+
   return res.render('users/views/assignDigipass', {
     csrfToken: req.csrfToken(),
     user: req.session.k2sUser,
-    serialNumber: '',
+    serialNumber: req.session.digipassSerialNumberToAssign ? req.session.digipassSerialNumberToAssign : '',
     validationMessages: {},
   })
 };
