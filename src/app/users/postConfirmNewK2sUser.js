46405b478f4d62f350d8535da92297605c1d4396
Simon Bull <simon.bull.84@googlemail.com>
2018-02-13 15:29:40 +0000

    Test infrastructure calls when confirming invite

--- src/app/users/postConfirmNewK2sUser.js
+++ src/app/users/postConfirmNewK2sUser.js
@@ -8,7 +8,7 @@ const postConfirmNewK2sUser = async (req, res) => {
   }
 
   const invitationId = await createInvite(req.session.k2sUser.firstName, req.session.k2sUser.lastName, req.session.k2sUser.email, req.session.k2sUser.k2sId, req.session.digipassSerialNumberToAssign, req.id);
-  await addInvitationService(invitationId, req.session.k2sUser.localAuthority, config.serviceMapping.key2SuccessServiceId, 0, req.id)
+  await addInvitationService(invitationId, req.session.k2sUser.localAuthority, config.serviceMapping.key2SuccessServiceId, 0, req.id);
 
   res.flash('info', `${req.session.k2sUser.firstName} ${req.session.k2sUser.lastName} has been invited to join Key to Success.`);
   console.log('set flash');
