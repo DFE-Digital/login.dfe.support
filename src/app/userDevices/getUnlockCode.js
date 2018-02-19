9f6cf6b35e972fa9ec798dd5d21b0b823df688e3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-19 11:12:50 +0000

    remove not needed call to get token details

--- src/app/userDevices/getUnlockCode.js
+++ src/app/userDevices/getUnlockCode.js
@@ -3,8 +3,6 @@ const { sendResult } = require('./../../infrastructure/utils');
 
 const action = async (req, res) => {
 
-  const tokenDetails = await getUserTokenDetails(req, req.params);
-
   sendResult(req, res, 'userDevices/views/getUnlockCode', {
     csrfToken: req.csrfToken(),
     uid: req.params.uid,
