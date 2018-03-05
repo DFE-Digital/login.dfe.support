10bd9659f81eee9050d485cb514dc03b5f9ff740
Simon Bull <simon.bull.84@googlemail.com>
2018-03-05 11:30:52 +0000

    correctly avoid attempting to de-jsonify unneccassrily

--- src/app/users/utils.js
+++ src/app/users/utils.js
@@ -24,9 +24,10 @@ const auditDateFixer = (audit) => {
 };
 const patchChangeHistory = (changeHistory) => {
   changeHistory.audits = changeHistory.audits.map((audit) => {
-    if (audit.editedFields && typeof(audit.editedFields) !== 'array') {
+    if (audit.editedFields && !(audit.editedFields instanceof Array)) {
       audit.editedFields = JSON.parse(audit.editedFields);
     }
+    return audit;
   });
 };
 
