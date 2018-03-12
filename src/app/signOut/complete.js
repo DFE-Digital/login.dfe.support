07c0bce795bda29db558bf596d744a1e0438689a
James King <hello@jamesmichaelking.com>
2018-03-12 12:29:19 +0000

    Add sign-out link

--- src/app/signOut/complete.js
+++ src/app/signOut/complete.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const complete = (req, res) => {
-  res.render('signOut/views/complete');
+  res.render('signOut/views/complete', {
+    hideSignOut: true
+  });
 };
 
 module.exports = complete;
