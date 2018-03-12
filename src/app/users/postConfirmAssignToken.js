d04a819c584fb6f02f64ac6a8f6bef1f7965cb75
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-12 07:52:30 +0000

    Add confirm digipass to user action

---
+++ src/app/users/postConfirmAssignToken.js
@@ -0,0 +1,26 @@
+const { createDevice } = require('./utils');
+const { sendResult } = require('./../../infrastructure/utils');
+
+const postConfirmAssignToken = async (req, res) => {
+  if (!req.body.userId || !req.body.serialNumber) {
+    return res.redirect('../');
+  }
+
+  const result = createDevice(req);
+
+  if(!result) {
+    sendResult(req, res, 'users/views/assignDigipass', {
+      csrfToken: req.csrfToken(),
+      validationMessages: {
+        error: 'Failed to assign token to user',
+      },
+      uid: req.body.userId,
+      serialNumber: req.body.serialNumber,
+    });
+  }
+
+  res.flash('info', `New token assigned Token ${req.session.digipassSerialNumberToAssign} has been assigned to this user.`);
+  return res.redirect(`/users/${req.body.userId}`);
+};
+
+module.exports = postConfirmAssignToken;
