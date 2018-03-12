de8efb5695657a7ca853b14e84970ee195cb08aa
James King <hello@jamesmichaelking.com>
2018-03-12 12:15:10 +0000

    Added signout functionality

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const users = require('./app/users');
 const userDevices = require('./app/userDevices');
 const errors = require('./app/errors');
+const signOut = require('./app/signOut');
 const healthCheck = require('login.dfe.healthcheck');
 const { getHealthCheckChecks } = require('./infrastructure/healthCheck');
 const config = require('./infrastructure/config');
@@ -11,6 +12,7 @@ const routes = (app, csrf) => {
 
   app.use('/users', users(csrf));
   app.use('/userDevices', userDevices(csrf));
+  app.use('/signout', signOut(csrf));
   app.use('/', errors(csrf));
 
   app.get('/', (req, res) => {
