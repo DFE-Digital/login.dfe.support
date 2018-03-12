26bd56a21772a0dabc95bf2d8f6580d3a5f5fd98
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-12 08:12:50 +0000

    Add missing await and update view

--- src/app/users/getConfirmAssignToken.js
+++ src/app/users/getConfirmAssignToken.js
@@ -3,7 +3,9 @@ const getAssignDigipass = (req, res) => {
   return res.render('users/views/confirmDigipass', {
     csrfToken: req.csrfToken(),
     userId: req.params.uid,
-    serialNumber: req.params.serialNumber,
+    email: req.session.user.email,
+    user: req.session.user,
+    serialNumber: req.session.digipassSerialNumberToAssign,
     validationMessages: {},
   })
 };
