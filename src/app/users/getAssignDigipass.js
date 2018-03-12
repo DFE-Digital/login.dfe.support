641e0bf76bf33c7c41ccf883476699731824ce51
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-12 06:33:02 +0000

    Update assign digipass to have new flow
    New flow available from route of existing user to assign token

--- src/app/users/getAssignDigipass.js
+++ src/app/users/getAssignDigipass.js
@@ -1,13 +1,29 @@
-const getAssignDigipass = (req, res) => {
-  if (!req.session.k2sUser) {
+const {getUserDetails} = require('./../users/utils');
+
+const getAssignDigipass = async (req, res) => {
+  let user;
+  let isExistingUser = true;
+
+  if (req.session.k2sUser) {
+    user = req.session.k2sUser;
+    isExistingUser = false;
+  }
+
+  if(req.params.uid) {
+    user = await getUserDetails(req);
+    req.session.user = user;
+  }
+
+  if(!user) {
     return res.redirect('../');
   }
 
   return res.render('users/views/assignDigipass', {
     csrfToken: req.csrfToken(),
-    user: req.session.k2sUser,
+    user,
     serialNumber: req.session.digipassSerialNumberToAssign ? req.session.digipassSerialNumberToAssign : '',
     validationMessages: {},
+    isExistingUser,
   })
 };
 
