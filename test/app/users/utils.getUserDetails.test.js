7922d4a51ca98751106030b1120276b8c63d040e
Simon Bull <simon.bull.84@googlemail.com>
2018-03-15 21:26:17 +0000

    FIx test for getUserDetails

--- test/app/users/utils.getUserDetails.test.js
+++ test/app/users/utils.getUserDetails.test.js
@@ -2,10 +2,12 @@ jest.mock('./../../../src/infrastructure/config', () => require('./../../utils')
 jest.mock('./../../../src/infrastructure/directories');
 jest.mock('./../../../src/infrastructure/organisations');
 jest.mock('./../../../src/infrastructure/audit');
+jest.mock('./../../../src/infrastructure/users');
 
 const { getUser } = require('./../../../src/infrastructure/directories');
 const { getServicesByUserId } = require('./../../../src/infrastructure/organisations');
 const { getUserLoginAuditsSince, getUserChangeHistory } = require('./../../../src/infrastructure/audit');
+const users = require('./../../../src/infrastructure/users');
 const { getUserDetails } = require('./../../../src/app/users/utils');
 
 describe('When getting user details', () => {
@@ -14,72 +16,21 @@ describe('When getting user details', () => {
   beforeEach(() => {
     getServicesByUserId.mockReset();
 
-
-    getUser.mockReset();
-    getUser.mockReturnValue({
-      sub: 'user1',
-      given_name: 'Albus',
-      family_name: 'Dumbledore',
+    users.getById.mockReset();
+    users.getById.mockReturnValue({
+      id: 'user1',
+      name: 'Albus Dumbledore',
+      firstName: 'Albus',
+      lastName: 'Dumbledore',
       email: 'headmaster@hogwarts.com',
-      password: '0dqy81MVA9lqs2+xinvOXbbGMhd18X4pq5aRfiE65pIKxcWB0OtAffY9NdJy0/ksjhBG9EOYywti2WYmtqwxypRil+x0/nBeBlJUfN7/Q9l8tRiDcqq8NghC8wqSEuyzLKXoE/+VDPkW35Vo8czsOp5PT0xN3IQ31vlld/4PqsqQWYE4WBTBO/PO6SoAfNapDxb4M9C8TiReek43pfVL3wTst8Bv4wkeFcLy7NMGVyM48LmjlyvYPIY5NTz8RGOSCAyB7kIxYEsf9SB0Sp0IMGhHIoM8/Yhso3cJNTKTLod0Uz3Htc0JAStugt6RCrnar3Yc7yUzSGDNZcvM31HsP74i5TifaJiavHOiZxjaHYn/KsLFi5/zqNRcYkzN+dYzWY1hjCSY47za9HMh89ZHxGkmrknQY4YKRp/uvg2driXwZDaIm7NUt90mXim4PGM0kYejp9SUwlIGmc5F4QO5F3tBoRb/AYsf3f6mDw7SXAMnO/OVfglvf/x3ICE7UCLkuMXZAECe8MJoJnpP+LVrNQfJjSrjmBYrVRVkS2QFrte0g2WO1SprE9KH8kkmNEmkC6Z3orDczx5jW7LSl37ZHzq1dvMYAJrEoWH21e6ug5usMSl1X6S5uBIsSrj8kOlTYgr4huPjN54aBTVYazCn6UFVrt83E81nbuyZTadrnA4=',
-      salt: 'PasswordIs-password-',
-    });
-
-    getUserLoginAuditsSince.mockReset();
-    getUserLoginAuditsSince.mockReturnValue([
-      {
-        type: "sign-in",
-        subType: "username-password",
-        success: true,
-        userId: "user1",
-        userEmail: "headmaster@hogwarts.com",
-        level: "audit",
-        message: "Successful login attempt for headmaster@hogwarts.com (id: user1)",
-        timestamp: "2017-10-24T11:35:51.633Z"
+      organisation: null,
+      lastLogin: new Date('2017-10-24T12:35:51.633Z'),
+      successfulLoginsInPast12Months: 2,
+      status: {
+        id: 1,
+        description: 'Active',
+        changedOn: new Date("2017-10-24T12:35:51.633Z"),
       },
-      {
-        type: "sign-in",
-        subType: "username-password",
-        success: true,
-        userId: "user1",
-        userEmail: "headmaster@hogwarts.com",
-        level: "audit",
-        message: "Successful login attempt for headmaster@hogwarts.com (id: user1)",
-        timestamp: "2017-10-24T12:35:51.633Z"
-      },
-      {
-        type: "sign-in",
-        subType: "username-password",
-        success: false,
-        userId: "user1",
-        userEmail: "headmaster@hogwarts.com",
-        level: "audit",
-        message: "Failed login attempt for headmaster@hogwarts.com (id: user1)",
-        timestamp: "2017-10-24T10:35:51.633Z"
-      }
-    ]);
-
-    getUserChangeHistory.mockReset();
-    getUserChangeHistory.mockReturnValue({
-      audits: [{
-        type: 'support',
-        subType: 'user-edit',
-        userId: "7a1b077a-d7d4-4b60-83e8-1a1b49849510",
-        userEmail: "some.user@test.tester",
-        editedUser: 'user1',
-        editedFields: [
-          {
-            name: 'status',
-            oldValue: 1,
-            newValue: 0,
-          }
-        ],
-        level: "audit",
-        message: "Successful login attempt for headmaster@hogwarts.com (id: user1)",
-        timestamp: "2017-10-24T12:35:51.633Z"
-      }],
-      numberOfPages: 1,
-      numberOfRecords: 1,
     });
 
     req = {
@@ -89,18 +40,11 @@ describe('When getting user details', () => {
     };
   });
 
-  it('then it should get user from directories', async () => {
+  it('then it should get user from users index', async () => {
     await getUserDetails(req);
 
-    expect(getUser.mock.calls).toHaveLength(1);
-    expect(getUser.mock.calls[0][0]).toBe('user1');
-  });
-
-  it('then it should get user logins from audit', async () => {
-    await getUserDetails(req);
-
-    expect(getUserLoginAuditsSince.mock.calls).toHaveLength(1);
-    expect(getUserLoginAuditsSince.mock.calls[0][0]).toBe('user1');
+    expect(users.getById.mock.calls).toHaveLength(1);
+    expect(users.getById.mock.calls[0][0]).toBe('user1');
   });
 
   it('then it should map user and login data to result', async () => {
@@ -108,6 +52,8 @@ describe('When getting user details', () => {
 
     expect(actual).toMatchObject({
       name: 'Albus Dumbledore',
+      firstName: 'Albus',
+      lastName: 'Dumbledore',
       email: 'headmaster@hogwarts.com',
       lastLogin: new Date('2017-10-24T12:35:51.633Z'),
       status: {
