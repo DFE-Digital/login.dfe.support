49b5023f19729f77de908799db624dccf3a3c391
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-10 09:38:43 +0100

    Add organisation name search to user index

--- src/infrastructure/users/azureSearch.js
+++ src/infrastructure/users/azureSearch.js
@@ -174,6 +174,7 @@ const createIndex = async () => {
           { name: 'email', type: 'Edm.String', sortable: true, filterable: true },
           { name: 'emailSearch', type: 'Edm.String', searchable: true },
           { name: 'organisationName', type: 'Edm.String', sortable: true, filterable: true },
+          { name: 'organisationNameSearch', type: 'Edm.String', searchable: true },
           { name: 'organisationCategories', type: 'Collection(Edm.String)', searchable: false, filterable: true },
           { name: 'services', type: 'Collection(Edm.String)', searchable: false, filterable: true },
           { name: 'lastLogin', type: 'Edm.Int64', sortable: true, filterable: true },
