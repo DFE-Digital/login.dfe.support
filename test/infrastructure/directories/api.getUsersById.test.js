0efc5af0fbcc24ae50e376224d1239936970f6bf
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-06-11 08:00:46 +0100

    Add call to get users by ids from directories API

---
+++ test/infrastructure/directories/api.getUsersById.test.js
@@ -0,0 +1,70 @@
+jest.mock('request-promise');
+jest.mock('login.dfe.jwt-strategies');
+jest.mock('./../../../src/infrastructure/config', () => require('./../../utils').configMockFactory({
+  directories: {
+    type: 'api',
+    service: {
+      url: 'http://directories.test',
+    },
+  },
+}));
+
+const rp = jest.fn();
+const requestPromise = require('request-promise');
+requestPromise.defaults.mockReturnValue(rp);
+
+const jwtStrategy = require('login.dfe.jwt-strategies');
+const { getUsersById } = require('./../../../src/infrastructure/directories/api');
+
+const correlationId = 'abc123';
+const apiResponse = {
+  users: [],
+  numberOfPages: 1,
+};
+const userIds = ['user1', 'user2'];
+
+describe('when getting a page of users from directories api', () => {
+  beforeEach(() => {
+    rp.mockReset();
+    rp.mockImplementation(() => {
+      return apiResponse;
+    });
+
+    jwtStrategy.mockReset();
+    jwtStrategy.mockImplementation(() => {
+      return {
+        getBearerToken: jest.fn().mockReturnValue('token'),
+      };
+    })
+  });
+
+  it('then it calls directories api with Ids', async () => {
+    await getUsersById(userIds, correlationId);
+
+    expect(rp.mock.calls).toHaveLength(1);
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      method: 'GET',
+      uri: 'http://directories.test/users/by-ids?id=user1,user2',
+    });
+  });
+
+  it('then it should use the token from jwt strategy as bearer token', async () => {
+    await getUsersById(userIds, correlationId);
+
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      headers: {
+        authorization: 'bearer token',
+      },
+    });
+  });
+
+  it('then it should include the correlation id', async () => {
+    await getUsersById(userIds, correlationId);
+
+    expect(rp.mock.calls[0][0]).toMatchObject({
+      headers: {
+        'x-correlation-id': correlationId,
+      },
+    });
+  });
+});
