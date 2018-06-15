6adca8ba5ded58f9dd58e06fdf374b7f932e08e9
James King <hello@jamesmichaelking.com>
2018-06-15 12:00:35 +0100

    Always pass role and don’t throw an error if we have no role

--- src/app/users/getConfirmNewUser.js
+++ src/app/users/getConfirmNewUser.js
@@ -5,7 +5,7 @@ const getRoleName = (id) => {
     case 10000:
       return 'Approver';
     default:
-      throw new Error(`Unrecognised role ${id}`);
+      return null;
   }
 };
 
@@ -21,10 +21,10 @@ const getConfirmNewUser = (req, res) => {
       id: req.session.user.organisationId,
       name: req.session.user.organisationName,
     } : undefined,
-    role:req.session.user.permission ? {
+    role: {
       id: req.session.user.permission,
       name: getRoleName(req.session.user.permission),
-    } : undefined,
+    },
   });
 };
 
