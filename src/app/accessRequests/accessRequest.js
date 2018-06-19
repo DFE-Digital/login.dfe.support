85029e513acec7bac0633855de0cb4dfe3fc542d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-06-19 08:05:17 +0100

    Add post action for responding to access request

--- src/app/accessRequests/accessRequest.js
+++ src/app/accessRequests/accessRequest.js
@@ -1,6 +1,6 @@
 'use strict';
 const { sendResult } = require('./../../infrastructure/utils');
-const { getById } = require('./utils');
+const { getById, putUserInOrganisation } = require('./utils');
 
 const get = async (req, res) => {
   const accessRequest = await getById(req);
@@ -13,6 +13,20 @@ const get = async (req, res) => {
   sendResult(req, res, 'accessRequests/views/request', model);
 };
 
+const post = async (req, res) => {
+
+  await putUserInOrganisation(req);
+
+  if(req.body.approve_reject.toLowerCase() === 'approve') {
+    res.flash('info', `Access request approved. ${req.body.name} is now associated with ${req.body.org_name}`);
+  } else {
+    res.flash('info', `Access request rejected. ${req.body.name} has been rejected and audited.`);
+  }
+
+  res.redirect('/access-requests');
+};
+
 module.exports = {
   get,
+  post,
 };
