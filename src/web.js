025a5995c199ea9276f72f300e264b1581ebde0b
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-07-17 09:42:37 +0100

    NSA-1622-sanitization invite user
    exclude first name and last name from sanitization when inviting user in support

--- src/web.js
+++ src/web.js
@@ -90,7 +90,7 @@ const init = async () => {
   app.use(cookieParser());
   app.use(sanitization({
     sanitizer: (key, value) => {
-      const fieldToNotSanitize = ['criteria', 'email'];
+      const fieldToNotSanitize = ['criteria', 'email', 'firstName', 'lastName'];
       if (fieldToNotSanitize.find(x => x.toLowerCase() === key.toLowerCase())) {
         return value;
       }
