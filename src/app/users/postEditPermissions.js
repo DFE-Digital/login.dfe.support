804d6a0c682fd38baff7fc851f5359d9edff972e
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-06 15:05:12 +0100

    NSA-1487 edit permissions
    Support invited users edit permissions

--- src/app/users/postEditPermissions.js
+++ src/app/users/postEditPermissions.js
@@ -1,4 +1,4 @@
-const { setUserAccessToOrganisation } = require('./../../infrastructure/organisations');
+const { setUserAccessToOrganisation, addInvitationOrganisation } = require('./../../infrastructure/organisations');
 const validatePermissions = (req) => {
   const validPermissions = [0, 10000];
   const level = parseInt(req.body.selectedLevel);
@@ -16,6 +16,22 @@ const validatePermissions = (req) => {
   return model;
 };
 
+const editInvitationPermissions = async(uid, req) => {
+  const model = validatePermissions(req);
+  const invitationId = uid.substr(4);
+  const organisationId = req.params.id;
+  const permissionId = model.selectedLevel;
+  await addInvitationOrganisation(invitationId, organisationId, permissionId, req.id);
+};
+
+const editUserPermissions = async(uid, req) => {
+  const model = validatePermissions(req);
+  const organisationId = req.params.id;
+  const permissionId = model.selectedLevel;
+
+  await setUserAccessToOrganisation(uid, organisationId, permissionId, req.id);
+};
+
 
 const postEditPermissions = async (req, res) => {
   const model = validatePermissions(req);
@@ -23,16 +39,17 @@ const postEditPermissions = async (req, res) => {
     model.csrfToken = req.csrfToken();
     return res.render('users/views/editPermissions', model);
   }
-  const userId = req.params.uid;
-  const organisationId = req.params.id;
+  const uid = req.params.uid;
+  if (uid.startsWith('inv-')) {
+    await editInvitationPermissions(uid, req);
+  } else {
+    await editUserPermissions(uid, req);
+  }
   const fullname = model.userFullName;
   const organisationName = model.organisationName;
-  const permissionId = model.selectedLevel;
-
-  await setUserAccessToOrganisation(userId, organisationId, permissionId, req.id);
-  const permissionName = permissionId === 10000 ? 'approver' : 'end user';
+  const permissionName = model.selectedLevel === 10000 ? 'approver' : 'end user';
   res.flash('info', `${fullname} now has ${permissionName} access to ${organisationName} `);
-  return res.redirect(`/users/${userId}/organisations`);
+  return res.redirect(`/users/${uid}/organisations`);
 };
 
 module.exports = postEditPermissions;
