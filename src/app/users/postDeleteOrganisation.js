10835f5dc9424a178e5cb2520f852cc1eb0c0efc
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-14 16:50:51 +0100

    NSA-1488 delete org

---
+++ src/app/users/postDeleteOrganisation.js
@@ -0,0 +1,26 @@
+const { deleteUserOrganisation, deleteInvitationOrganisation } = require('./../../infrastructure/organisations');
+
+const deleteInvitationOrg = async (uid, req) => {
+  const invitationId =  uid.substr(4);
+  const organisationId = req.params.id;
+  await deleteInvitationOrganisation(invitationId, organisationId);
+};
+
+const deleteUserOrg = async (uid, req) => {
+  const organisationId = req.params.id;
+  await deleteUserOrganisation(uid, organisationId);
+};
+
+const postDeleteOrganisation = async (req, res) => {
+  const uid = req.params.uid;
+  if (uid.startsWith('inv-')) {
+    await deleteInvitationOrg(uid, req);
+  } else {
+    await deleteUserOrg(uid, req);
+  }
+  res.flash('info', 'Deactivate complete - Organisation has been deactivated');
+  return res.redirect('../');
+};
+
+
+module.exports = postDeleteOrganisation;
