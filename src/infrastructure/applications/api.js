5fda026ea94c3fb9d6531286530a635cf10e6915
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-21 10:26:21 +0100

    Get service by id

---
+++ src/infrastructure/applications/api.js
@@ -0,0 +1,60 @@
+const config = require('./../config');
+const KeepAliveAgent = require('agentkeepalive').HttpsAgent;
+
+const rp = require('request-promise').defaults({
+  agent: new KeepAliveAgent({
+    maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+    maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+    timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+    keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
+  }),
+});
+const jwtStrategy = require('login.dfe.jwt-strategies');
+
+const getServiceById = async (id) => {
+  if (!id) {
+    return undefined;
+  }
+  const token = await jwtStrategy(config.applications.service).getBearerToken();
+  try {
+    const client = await rp({
+      method: 'GET',
+      uri: `${config.applications.service.url}/services/${id}`,
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+      json: true,
+    });
+    return client;
+  } catch (e) {
+    if (e.statusCode === 404) {
+      return undefined;
+    }
+    throw e;
+  }
+};
+
+const getAll = async () => {
+  const token = await jwtStrategy(config.applications.service).getBearerToken();
+  try {
+    const client = await rp({
+      method: 'GET',
+      uri: `${config.applications.service.url}/services`,
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+      json: true,
+    });
+    return client;
+  } catch (e) {
+    if (e.statusCode === 404) {
+      return undefined;
+    }
+    throw e;
+  }
+};
+
+module.exports = {
+  getServiceById,
+  getAll,
+};
