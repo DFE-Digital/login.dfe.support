79ea2979c157df824b61758e32b1b9cb96fcefe0
Simon Bull <simon.bull.84@googlemail.com>
2018-09-11 10:46:58 +0100

    Update request-promise to login.dfe.request-promise-retry for directories and orgs

--- test/infrastructure/organisations/api.getOrganisationUsersForApproval.test.js
+++ test/infrastructure/organisations/api.getOrganisationUsersForApproval.test.js
@@ -1,4 +1,4 @@
-jest.mock('request-promise');
+jest.mock('login.dfe.request-promise-retry');
 jest.mock('agentkeepalive', () => {
   return {
     HttpsAgent : jest.fn()
@@ -15,7 +15,7 @@ jest.mock('./../../../src/infrastructure/config', () => require('./../../utils')
 }));
 
 const rp = jest.fn();
-const requestPromise = require('request-promise');
+const requestPromise = require('login.dfe.request-promise-retry');
 requestPromise.defaults.mockReturnValue(rp);
 
 const jwtStrategy = require('login.dfe.jwt-strategies');
