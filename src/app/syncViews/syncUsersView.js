7a5a3a1a7f6afca0041ae8fd61e703c85dcd778c
Simon Bull <simon.bull.84@googlemail.com>
2018-10-09 21:38:50 +0100

    update defaults for index and fix invitation service model

--- src/app/syncViews/syncUsersView.js
+++ src/app/syncViews/syncUsersView.js
@@ -137,12 +137,14 @@ const buildUsersThatHaveChanged = async (correlationId) => {
   const updatedUsers = await getAllUsers(changedAfter, correlationId);
   const userServices = [];
 
+  logger.info(`Reading service mappings for users (correlationId: ${correlationId})`, { correlationId });
   for (let i = 0; i < updatedUsers.length; i++) {
     const serviceMapping = await organisations.getServicesByUserId(updatedUsers[i].sub, correlationId);
     if (serviceMapping && serviceMapping.length > 0) {
       userServices.push(...serviceMapping);
     }
   }
+  logger.info(`Finished reading service mappings for users (correlationId: ${correlationId})`, { correlationId });
 
   return buildUsers(updatedUsers, userServices);
 };
@@ -224,10 +226,13 @@ const buildInvitationsThatHaveChanged = async (correlationId) => {
   const invitationServices = [];
 
   for (let i = 0; i < updatedInvitations.length; i++) {
-    const serviceMapping = await organisations.getInvitationOrganisations(updatedInvitations[i].id, correlationId);
-    if (serviceMapping && serviceMapping.length > 0) {
-      invitationServices.push(...serviceMapping);
-    }
+    const serviceMapping = (await organisations.getInvitationOrganisations(updatedInvitations[i].id, correlationId)) || [];
+    serviceMapping.forEach((mapping) => {
+      mapping.services.forEach((svc) => {
+        const mapped = Object.assign({ invitationId: updatedInvitations[i].id }, svc, { organisation: mapping.organisation });
+        invitationServices.push(mapped);
+      });
+    });
   }
 
   return buildInvitations(updatedInvitations, invitationServices);
