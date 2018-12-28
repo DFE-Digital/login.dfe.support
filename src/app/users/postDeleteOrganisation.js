23fe0ae7177f36e288a0d4fa6c945c2a4ae18758
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-12-28 11:56:01 +0000

    NSA-1955- remove services before removing org

--- src/app/users/postDeleteOrganisation.js
+++ src/app/users/postDeleteOrganisation.js
@@ -1,5 +1,7 @@
 const logger = require('./../../infrastructure/logger');
 const { deleteUserOrganisation, deleteInvitationOrganisation } = require('./../../infrastructure/organisations');
+const { getAllServicesForUserInOrg } = require('./utils');
+const { removeServiceFromInvitation, removeServiceFromUser } = require('./../../infrastructure/access');
 
 const deleteInvitationOrg = async (uid, req) => {
   const invitationId =  uid.substr(4);
@@ -14,11 +16,23 @@ const deleteUserOrg = async (uid, req) => {
 
 const postDeleteOrganisation = async (req, res) => {
   const uid = req.params.uid;
+  const organisationId = req.params.id;
+  const servicesForUserInOrg = await getAllServicesForUserInOrg(uid, organisationId, req.id);
+
   if (uid.startsWith('inv-')) {
+    for (let i = 0; i < servicesForUserInOrg.length; i++) {
+      const service = servicesForUserInOrg[i];
+      await removeServiceFromInvitation(uid.substr(4), service.id, organisationId, req.id);
+    }
     await deleteInvitationOrg(uid, req);
   } else {
+    for (let i = 0; i < servicesForUserInOrg.length; i++) {
+      const service = servicesForUserInOrg[i];
+      await removeServiceFromUser(uid, service.id, organisationId, req.id);
+    }
     await deleteUserOrg(uid, req);
   }
+
   const fullname = `${req.session.user.firstName} ${req.session.user.lastName}`;
   const org = req.session.org.name;
   logger.audit(`${req.user.email} (id: ${req.user.sub}) removed organisation ${org} (id: ${req.params.id}) for user ${req.session.user.email} (id: ${uid})`, {
