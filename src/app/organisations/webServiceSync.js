23b5644017fd18767aa9612e821bc4fd3e1cdf42
Simon Bull <simon.bull.84@googlemail.com>
2019-02-11 09:54:31 +0000

    Send org for ws sync

---
+++ src/app/organisations/webServiceSync.js
@@ -0,0 +1,26 @@
+const config = require('./../../infrastructure/config');
+const { sendResult } = require('./../../infrastructure/utils');
+const { getOrganisationByIdV2 } = require('./../../infrastructure/organisations');
+const ServiceNotificationsClient = require('login.dfe.service-notifications.jobs.client');
+
+const get = async (req, res) => {
+  const organisation = await getOrganisationByIdV2(req.params.id, req.id);
+
+  sendResult(req, res, 'organisations/views/webServiceSync', {
+    csrfToken: req.csrfToken(),
+    organisation: organisation,
+  });
+};
+const post = async (req, res) => {
+  const organisation = await getOrganisationByIdV2(req.params.id, req.id);
+
+  const serviceNotificationsClient = new ServiceNotificationsClient(config.notifications);
+  await serviceNotificationsClient.notifyOrganisationUpdated(organisation);
+
+  res.flash('info', 'Organisation has been queued for sync');
+  return res.redirect(`/organisations/${organisation.id}/users`);
+};
+module.exports = {
+  get,
+  post,
+};
