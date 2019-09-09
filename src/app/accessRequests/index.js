bd277ce7d2096e7647054fe3d5c148a26aab9571
Steve O'Hara <github@soharadesign.com>
2019-09-09 18:04:28 +0100

    [NSA-2249] Altered new access-request routes to use the unused originals

--- src/app/accessRequests/index.js
+++ src/app/accessRequests/index.js
@@ -8,19 +8,28 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 const {get: getSearch,post: postSearch} = require('./search');
 const {get: getRequest, post: postRequest} = require('./accessRequest');
 
+const getOrganisationRequests = require('./getOrganisationRequests');
+const { get: getReviewOrganisationRequest, post: postReviewOrganisationRequest } = require('./reviewOrganisationRequest');
+const { get: getRejectOrganisationRequest, post: postRejectOrganisationRequest } = require('./rejectOrganisationRequest');
+
 const router = express.Router({ mergeParams: true });
 
 const users = (csrf) => {
-  logger.info('Mounting access requests routes');
+  logger.info('Mounting accessRequests routes');
 
   router.use(isLoggedIn);
-  router.use(setCurrentArea('accessrequests'));
+  router.use(setCurrentArea('accessRequests'));
 
-  router.get('/', csrf, asyncWrapper(getSearch));
-  router.post('/', csrf, asyncWrapper(postSearch));
+  router.get('/', csrf, asyncWrapper(getOrganisationRequests));
 
   router.get('/request/:id', csrf, asyncWrapper(getRequest));
   router.post('/request/:id', csrf, asyncWrapper(postRequest));
+
+  // Approve/Reject org requests
+  router.get('/:orgId/requests/:rid', csrf, asyncWrapper(getReviewOrganisationRequest));
+  router.post('/:orgId/requests/:rid', csrf, asyncWrapper(postReviewOrganisationRequest));
+  router.get('/:orgId/requests/:rid/rejected', csrf, asyncWrapper(getRejectOrganisationRequest));
+  router.post('/:orgId/requests/:rid/rejected', csrf, asyncWrapper(postRejectOrganisationRequest));
 
   return router;
 };
