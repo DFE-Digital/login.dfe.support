66d87a2f8dc1125d11ee619f120c29f492f5a91e
Steve O'Hara <github@soharadesign.com>
2019-09-18 20:29:29 +0100

    [NSA-2250] Implemented POST for org request reject journey

--- src/app/accessRequests/rejectOrganisationRequest.js
+++ src/app/accessRequests/rejectOrganisationRequest.js
@@ -19,6 +19,34 @@ const get = async (req, res) => {
   })
 };
 
+const post = async (req, res) => {
+  if (Object.keys(model.validationMessages).length > 0) {
+    model.csrfToken = req.csrfToken();
+    return res.render('accessRequests/views/rejectOrganisationRequest', model);
+  }
+  // patch request with rejection
+  const actionedDate = Date.now();
+  await updateRequestById(model.request.id, -1, req.user.sub, model.reason, actionedDate, req.id);
+
+  //send rejected email
+  await notificationClient.sendAccessRequest(model.request.usersEmail, model.request.usersName, model.request.org_name, false, null);
+
+  //audit organisation rejected
+  logger.audit(`${req.user.email} (id: ${req.user.sub}) rejected organisation request for ${model.request.org_id})`, {
+    type: 'approver',
+    subType: 'rejected-org',
+    userId: req.user.sub,
+    editedUser: model.request.user_id,
+    reason: model.reason,
+  });
+
+  res.flash('info', `Request rejected - an email has been sent to ${model.request.usersEmail}.`);
+  return res.redirect(`/access-requests`);
+
+
+};
+
 module.exports = {
   get,
+  post,
 };
