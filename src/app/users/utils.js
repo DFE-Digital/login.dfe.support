c0dad06f527cf5733bf172804c1a7dce131cf068
Ronald Repka <ronald.repka@gmail.com>
2022-10-19 02:09:08 +0100

    NSA-5975 Sending modified Change org permission email

--- src/app/users/utils.js
+++ src/app/users/utils.js
@@ -289,6 +289,13 @@ const waitForIndexToUpdate = async (uid, updatedCheck) => {
   }
 };
 
+const mapRole = (roleId) => {
+  if (roleId === 10000) {
+    return { id: 10000, description: 'Approver' };
+  }
+  return { id: 0, description: 'End user' };
+};
+
 module.exports = {
   search,
   getUserDetails,
@@ -297,4 +304,5 @@ module.exports = {
   createDevice,
   waitForIndexToUpdate,
   getAllServicesForUserInOrg,
+  mapRole,
 };
