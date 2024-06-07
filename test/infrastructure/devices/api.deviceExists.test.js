36c209fbc919fd3416d705afe6ca24da22f9e45d
Richard Ward <richard.ward@methods.co.uk>
2024-06-07 13:47:12 +0100

    Improve the test implementation of `api.deviceExists.test.js`
    Create a mock for `fetch` which can be then be accessed to validate URI and headers based on preceding calls.
    
    Test Suites: 108 passed, 108 total
    Tests:       540 passed, 540 total

--- test/infrastructure/devices/api.deviceExists.test.js
+++ test/infrastructure/devices/api.deviceExists.test.js
@@ -20,12 +20,7 @@ const correlationId = 'abc123';
 describe('when getting a page of digipass tokens from the devices api', () => {
   
   const mockedFetch = jest.fn().mockResolvedValue({
-    method: 'GET',
-    uri: 'http://devices.test/digipass/123456789',
-    headers: {
-      authorization: 'bearer token',
-      'x-correlation-id': correlationId,
-    },
+    text: async () => 'Test response',
   });
 
   beforeEach(() => {
@@ -54,16 +49,20 @@ describe('when getting a page of digipass tokens from the devices api', () => {
 
     expect(asyncRetry.mock.calls).toHaveLength(1);
     
-    expect(await asyncRetry.mock.calls[0][0]()).toMatchObject({
+    await asyncRetry.mock.calls[0][0]();
+
+    expect(mockedFetch.mock.calls[0][0]).toBe('http://devices.test/digipass/123456789')
+    expect(mockedFetch.mock.calls[0][1]).toMatchObject({
       method: 'GET',
-      uri: 'http://devices.test/digipass/123456789'
     });
   });
 
   it('then it should use the token from jwt strategy as bearer token', async () => {
     await deviceExists('123456789', correlationId);
 
-    expect(await asyncRetry.mock.calls[0][0]()).toMatchObject({
+    await asyncRetry.mock.calls[0][0]();
+
+    expect(mockedFetch.mock.calls[0][1]).toMatchObject({
       headers: {
         authorization: 'bearer token',
       },
@@ -73,7 +72,9 @@ describe('when getting a page of digipass tokens from the devices api', () => {
   it('then it should include the correlation id', async () => {
     await deviceExists('123456789', correlationId);
 
-    expect(await asyncRetry.mock.calls[0][0]()).toMatchObject({
+    await asyncRetry.mock.calls[0][0]();
+
+    expect(mockedFetch.mock.calls[0][1]).toMatchObject({
       headers: {
         'x-correlation-id': correlationId,
       },
