b95835a6363e5e584f1e988c74c3542abf5c749f
Alex Platt <alex.platt@education.gov.uk>
2024-08-09 17:45:03 +0100

    Security trimmed the list of services to the current user

--- src/app/users/getManageConsoleServices.js
+++ src/app/users/getManageConsoleServices.js
@@ -1,10 +1,18 @@
 const { sendResult } = require('../../infrastructure/utils');
+const { getAllServices }= require('../../infrastructure/applications')
 const { getUserDetails } = require('./utils');
 const { getServicesByUserId } = require('./../../infrastructure/access');
 
 const getManageConsoleServices = async (req, res) => {
   const user = await getUserDetails(req);
-  const services = await getServicesByUserId(user.id);
+  const allServices = await getAllServices();
+  const userServices = await getServicesByUserId(user.id);
+
+  const filterIds = new Set(userServices.map(item => item.serviceId));
+
+  const services = allServices.services.filter(item => filterIds.has(item.id));
+
+
 
   sendResult(req, res, 'users/views/selectManageConsoleService', {
     csrfToken: req.csrfToken(),
