bee33bff80613e56cc4b4f8d5b3ce819d67f32b4
Alex Platt <alex.platt@education.gov.uk>
2024-08-19 14:11:06 +0100

    reverted back to all services shown in list for manage console services view

--- src/app/users/getManageConsoleServices.js
+++ src/app/users/getManageConsoleServices.js
@@ -1,24 +1,16 @@
 const { sendResult } = require('../../infrastructure/utils');
 const { getAllServices } = require('../../infrastructure/applications')
 const { getUserDetails } = require('./utils');
-const { getServicesByUserId } = require('./../../infrastructure/access');
 
 const getManageConsoleServices = async (req, res) => {
   const user = await getUserDetails(req);
   const allServices = await getAllServices();
-  const userServices = await getServicesByUserId(user.id);
-
-  let services = [];
-  if (userServices != undefined) {
-    const filterIds = new Set(userServices.map(item => item.serviceId));
-    services = allServices.services.filter(item => filterIds.has(item.id));
-  };
 
   sendResult(req, res, 'users/views/selectManageConsoleService', {
     csrfToken: req.csrfToken(),
     backLink: `/users/${user.id}/organisations`,
     user,
-    services
+    allServices
   });
 };
 
