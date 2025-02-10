043aa74aed265237400764c9c9d9a19f37077b16
Adam Mann <Adam.Mann@education.gov.uk>
2025-02-10 16:10:02 +0000

    First commit to get everything set up

---
+++ src/app/services/postChooseServiceType.js
@@ -0,0 +1,8 @@
+const postChooseServiceType = async (req, res) => {
+  // Validate Input
+  // Write input to sesssion
+  // Redirect to /users page (for now, until next page is done)
+  return res.redirect("/users");
+};
+
+module.exports = postChooseServiceType;
