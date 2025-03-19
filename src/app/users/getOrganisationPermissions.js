b7dba30644b6e833ac06946d0d00677db3f936ce
Alex Platt <alex.platt@education.gov.uk>
2025-03-19 09:50:22 +0000

    [NSA-8931] Convert organisation permission to invited user page in support to new GDS style

--- src/app/users/getOrganisationPermissions.js
+++ src/app/users/getOrganisationPermissions.js
@@ -1,6 +1,8 @@
 const getOrganisationPermissions = async (req, res) => {
   return res.render("users/views/organisationPermissions", {
     csrfToken: req.csrfToken(),
+    layout: "sharedViews/layoutNew.ejs",
+    backLink: "associate-organisation",
     userFullName: `${req.session.user.firstName} ${req.session.user.lastName}`,
     organisationName: req.session.user.organisationName,
     selectedLevel: req.session.user.permission || 0,
