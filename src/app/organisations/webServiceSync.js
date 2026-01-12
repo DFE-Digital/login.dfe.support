4b2d53448094cc0c6737e517a9067a12fd07312c
Adam Mann <Adam.Mann@education.gov.uk>
2026-01-12 13:11:12 +0000

    Improve output when sync error happens

--- src/app/organisations/webServiceSync.js
+++ src/app/organisations/webServiceSync.js
@@ -1,5 +1,7 @@
 const { sendResult } = require("../../infrastructure/utils");
+const logger = require("../../infrastructure/logger");
 const { getOrganisationRaw } = require("login.dfe.api-client/organisations");
+
 const { wsSyncCall } = require("./wsSynchFunCall");
 
 const get = async (req, res) => {
@@ -13,11 +15,18 @@ const get = async (req, res) => {
   });
 };
 const post = async (req, res) => {
-  await wsSyncCall(req.params.id);
-  const organisation = await getOrganisationRaw({
-    by: { organisationId: req.params.id },
-  });
-  return res.redirect(`/organisations/${organisation.id}/users`);
+  try {
+    const result = await wsSyncCall(req.params.id);
+    if (result === undefined) {
+      res.flash("info", "Organisation was not found when performing sync");
+      logger.info(`Sync returned 404 status for org [${req.params.id}].`);
+    }
+  } catch (e) {
+    res.flash("error", "Something went wrong during web service sync");
+    logger.error("Something went wrong during web service sync", e);
+  }
+
+  return res.redirect(`/organisations/${req.params.id}/users`);
 };
 module.exports = {
   get,
