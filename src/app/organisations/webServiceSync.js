1b9f67ab79293d1e93499e78afafc7aed5665060
Adam Mann <manny160389@hotmail.com>
2026-01-14 15:15:25 +0000

    [NSA-9588] Improve output when sync error happens (#643)
    * Improve output when sync error happens
    
    * PR comments
    
    * Tweak logging messaging

--- src/app/organisations/webServiceSync.js
+++ src/app/organisations/webServiceSync.js
@@ -1,5 +1,7 @@
 const { sendResult } = require("../../infrastructure/utils");
+const logger = require("../../infrastructure/logger");
 const { getOrganisationRaw } = require("login.dfe.api-client/organisations");
+
 const { wsSyncCall } = require("./wsSynchFunCall");
 
 const get = async (req, res) => {
@@ -13,11 +15,18 @@ const get = async (req, res) => {
   });
 };
 const post = async (req, res) => {
-  await wsSyncCall(req.params.id);
-  const organisation = await getOrganisationRaw({
-    by: { organisationId: req.params.id },
-  });
-  return res.redirect(`/organisations/${organisation.id}/users`);
+  try {
+    const result = await wsSyncCall(req.params.id);
+    if (result === undefined) {
+      res.flash("info", "No sync was performed, please check logs for details");
+      logger.info(`Sync call returned undefined for org [${req.params.id}].`);
+    }
+  } catch (e) {
+    res.flash("error", "Something went wrong during web service sync");
+    logger.error("Something went wrong during web service sync", e);
+  }
+
+  return res.redirect(`/organisations/${req.params.id}/users`);
 };
 module.exports = {
   get,
