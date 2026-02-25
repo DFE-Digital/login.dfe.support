a452aeaf1eb40d41d4d1e5fd95b7c464312360cc
Ramin Ahmadi <15119083+AhmadiRamin@users.noreply.github.com>
2026-02-25 09:43:15 +0000

    Fix jest config for api client mapping

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,12 @@ const config = {
   testEnvironment: "node",
   collectCoverage: false,
   clearMocks: true,
+  moduleNameMapper: {
+    "^login\\.dfe\\.api-client/api$":
+      "<rootDir>/node_modules/login.dfe.api-client/dist/api/index.js",
+    "^login\\.dfe\\.api-client/api/common/ApiClient$":
+      "<rootDir>/node_modules/login.dfe.api-client/dist/api/common/ApiClient.js",
+  },
 };
 
 module.exports = config;
