5f87773b986c6f6a6bd5ead53f6c5a3fd458deff
vsolomonadv <victor.solomon@advania.co.uk>
2026-05-18 09:19:40 +0100

    feat(NSA-9688): filter services with hideSupport from manage console service selection

--- test/app/users/getManageConsoleServices.test.js
+++ test/app/users/getManageConsoleServices.test.js
@@ -10,13 +10,9 @@ jest.mock("../../../src/app/services/utils", () => ({
 jest.mock("./../../../src/app/users/utils", () => ({
   getUserDetailsById: jest.fn(),
 }));
-jest.mock("login.dfe.api-client/services", () => ({
-  getPaginatedServicesRaw: jest.fn(),
-}));
 const getManageConsoleServices = require("./../../../src/app/users/getManageConsoleServices");
 const { sendResult } = require("./../../../src/infrastructure/utils");
 const { getAllServices } = require("../../../src/app/services/utils");
-const { getPaginatedServicesRaw } = require("login.dfe.api-client/services");
 const { getUserDetailsById } = require("./../../../src/app/users/utils");
 
 describe("When retrieving manage console services for a user", () => {
@@ -78,46 +74,26 @@ describe("When retrieving manage console services for a user", () => {
           isHiddenService: false,
           relyingParty: {},
         },
+        {
+          id: "hiddenServiceId",
+          name: "Hidden Service",
+          description: "A service hidden from support",
+          isExternalService: true,
+          isIdOnlyService: false,
+          isHiddenService: false,
+          relyingParty: {
+            params: {
+              hideApprover: "true",
+              hideSupport: "true",
+              helpHidden: "true",
+            },
+          },
+        },
       ],
     };
 
     getAllServices.mockReset();
     getAllServices.mockReturnValue(allServices);
-
-    const pageOfServices = {
-      services: [
-        {
-          id: "service1Id",
-          name: "Service 1",
-          description: "Service for testing purposes",
-          isExternalService: true,
-          isIdOnlyService: false,
-          isHiddenService: false,
-          relyingParty: {},
-        },
-        {
-          id: "service2Id",
-          name: "Service 2",
-          description: "Service for testing purposes",
-          isExternalService: true,
-          isIdOnlyService: false,
-          isHiddenService: false,
-          relyingParty: {},
-        },
-        {
-          id: "service3Id",
-          name: "Service 3",
-          description: "Service for testing purposes",
-          isExternalService: true,
-          isIdOnlyService: false,
-          isHiddenService: false,
-          relyingParty: {},
-        },
-      ],
-    };
-
-    getPaginatedServicesRaw.mockReset();
-    getPaginatedServicesRaw.mockReturnValue(pageOfServices);
   });
 
   it("should call getUserDetails", async () => {
@@ -130,54 +106,22 @@ describe("When retrieving manage console services for a user", () => {
     });
   });
 
-  it("should call getPaginatedServicesRaw", async () => {
+  it("should not include services where hideSupport is truthy", async () => {
     await getManageConsoleServices(req, res);
 
-    expect(getPaginatedServicesRaw).toHaveBeenCalled();
-    expect(getPaginatedServicesRaw).toHaveReturnedWith({
-      services: [
-        {
-          description: "Service for testing purposes",
-          id: "service1Id",
-          isExternalService: true,
-          isHiddenService: false,
-          isIdOnlyService: false,
-          name: "Service 1",
-          relyingParty: {},
-        },
-        {
-          description: "Service for testing purposes",
-          id: "service2Id",
-          isExternalService: true,
-          isHiddenService: false,
-          isIdOnlyService: false,
-          name: "Service 2",
-          relyingParty: {},
-        },
-        {
-          description: "Service for testing purposes",
-          id: "service3Id",
-          isExternalService: true,
-          isHiddenService: false,
-          isIdOnlyService: false,
-          name: "Service 3",
-          relyingParty: {},
-        },
-      ],
-    });
-
-    const getPaginatedServicesRawResult = getPaginatedServicesRaw();
-    expect(getPaginatedServicesRawResult.services[0].id).toBe("service1Id");
+    const pageServices = sendResult.mock.calls[0][3].pageOfServices.services;
+    expect(
+      pageServices.find((s) => s.id === "hiddenServiceId"),
+    ).toBeUndefined();
   });
 
-  it("should set pageOfServices to {services: []} if a undefined is returned from getPaginatedServicesRaw call", async () => {
-    getPaginatedServicesRaw.mockReturnValue(undefined);
-
+  it("should include services where hideSupport is not set", async () => {
     await getManageConsoleServices(req, res);
 
-    expect(sendResult.mock.calls[0][3].pageOfServices).toMatchObject({
-      services: [],
-    });
+    const pageServices = sendResult.mock.calls[0][3].pageOfServices.services;
+    expect(pageServices.find((s) => s.id === "service1Id")).toBeDefined();
+    expect(pageServices.find((s) => s.id === "service2Id")).toBeDefined();
+    expect(pageServices.find((s) => s.id === "service3Id")).toBeDefined();
   });
 
   it("should call sendResult", async () => {
